953a3c81f109f6dced28aa5c81142c59338e959b
Ray Elder <hello@rayelder.com>
2026-01-28 16:11:17 -0700

    Update import config for @rayelder/context

--- src/lib/components/Button/index.figma.tsx
+++ src/lib/components/Button/index.figma.tsx
@@ -1,19 +1,25 @@
-import React from "react"
-import Button from "./index"
-import figma from "@figma/code-connect"
+import React from 'react';
+import Button from './index';
+import figma from '@figma/code-connect';
 
 figma.connect(
   Button,
-  "https://www.figma.com/design/FNVYXYuplFl2mNZi9YzuQ9/Components?node-id=1%3A12",
+  'https://www.figma.com/design/FNVYXYuplFl2mNZi9YzuQ9/Components?node-id=1%3A12',
   {
     props: {
-      label: figma.string("label"),
-      variant: figma.enum("variant", {
-        primary: "primary",
-        secondary: "secondary",
+      label: figma.string('label'),
+      variant: figma.enum('variant', {
+        primary: 'primary',
+        secondary: 'secondary',
       }),
-      isPill: figma.boolean("isPill"),
+      isPill: figma.boolean('isPill'),
     },
-    example: (props) => <Button label={props.label} isPill={props.isPill} />,
+    example: (props) => (
+      <Button
+        label={props.label}
+        variant={props.variant}
+        isPill={props.isPill}
+      />
+    ),
   },
-)
+);
