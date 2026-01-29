3195fbc5134aa3420e67bf19c8d2238adc91daa2
Ray Elder <hello@rayelder.com>
2026-01-29 13:17:30 -0700

    Update homepage to preview components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,24 @@
-'use client'
+'use client';
 
-import styles from "./page.module.css";
-import Button from "@/lib/components/Button";
+import Link from '@/lib/components/Link';
+import styles from './page.module.css';
+import Button from '@/lib/components/Button';
+import ButtonGroup from '@/lib/components/ButtonGroup';
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-          <Button
-            label="Call-to-action"
-            isPill
-          />
-          <Button
-            label="Call-to-action"
-          />
-          <Button
-            label="Call-to-action"
-            variant="secondary"
-            isPill
-          />
-          <Button
-            label="Call-to-action"
-            variant="secondary"
-          />
+        <h1>Components</h1>
+        <h2>Button </h2>
+        <Button label="Sign in" isPill />
+        <h2>Link</h2>
+        <Link label="Browse contacts" />
+        <h2>ButtonGroup</h2>
+        <ButtonGroup>
+          <Button label="Sign in" isPill />
+          <Link label="Browse contacts" />
+        </ButtonGroup>
       </main>
     </div>
   );
